cbecf051e59c0315027f6155d6a8528680589caa
Anish Krishnankutty <Anish.Krishnankutty@standardbank.co.za>
2018-10-29 11:30:08 +0200

    testing12345

--- src/main/java/com/aws/codestar/projecttemplates/configuration/ApplicationConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/configuration/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package com.aws.codestar.projecttemplates.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
